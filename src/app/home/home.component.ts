98c090a521adc73697da8484c3cc24754e2d58e7
thebaron24 <thebaron24@gmail.com>
2019-01-16 23:43:58 -0500

    debugging content insertion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,9 @@ export class HomeComponent implements OnInit {
 	page: any[] = [];
 
 	constructor(private dataService: DataService) {
-		dataService.page$.subscribe(page => {this.page = page});
+		dataService.page$.subscribe(page => {
+			this.page = page;
+		});
 		dataService.config$.subscribe(config => {
       console.log("config is loaded");
       dataService.getPage('home');
