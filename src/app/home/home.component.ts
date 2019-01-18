dddc3149680782648b5055bb4059088c890f37ed
thebaron24 <thebaron24@gmail.com>
2019-01-18 18:02:44 -0500

    testing logic to destroy subscription while using onInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
+import { takeUntil } from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -8,21 +10,23 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
+	private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 	page: any[];
 
-	constructor(private dataService: DataService) {
-		this.dataService.page$.subscribe(page => {
+	constructor(private dataService: DataService) {}
+
+	ngOnInit(): void {
+		console.log("HomeComponent: OnInit firing");
+		this.dataService.page$.pipe(takeUntil(this.destroyed$)).subscribe(page => {
 			console.log("HomeComponent: page received - ", page)
 			this.page = page;
 		});
 	}
 
-	ngOnInit(): void {
-		console.log("HomeComponent: OnInit firing");
-	}
-
 	ngOnDestroy(): void {
 		console.log("HomeComponent: OnDestroy firing");
+		this.destroyed$.next(true);
+    this.destroyed$.complete();
 	}
 
 }
