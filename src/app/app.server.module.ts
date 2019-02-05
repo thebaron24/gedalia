f72f984086dd017cf32e7c18decb254202941b87
bwilson <bwilson@bridge2solutions.com>
2019-02-05 11:40:08 -0500

    added missing server transition state import to server module

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
+    ServerTransferStateModule,
     ModuleMapLoaderModule,
     MaterialModule
   ],
